b346fc80166208144c8e0f65d373d798a568a64e
Dan Kelly <dpk5e7@yahoo.com>
2022-09-23 16:16:53 -0600

    Added Fleet & Trip Models

---
+++ models/Trip.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Trip extends Model {}
+
+Trip.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    source_address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    destination_address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    electric_vehicle_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "electric_vehicle",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "trip",
+  }
+);
+
+module.exports = Trip;
