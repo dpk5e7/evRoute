c0ba690ee2101e109864d4f9a122b39146391002
Dan Kelly <dpk5e7@yahoo.com>
2022-09-23 12:08:12 -0600

    Worked on nrelRoutes and putting stations on the map

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const nrelRoutes = require("./nrelRoutes");
 
-router.use('/users', userRoutes);
+router.use("/users", userRoutes);
+router.use("/nrel", nrelRoutes);
 
 module.exports = router;
