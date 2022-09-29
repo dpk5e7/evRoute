97c31f6563cc136c380398211d87db531ba78fdd
Drew <dlederman69@gmail.com>
2022-09-29 14:31:47 -0600

    js/handlebars button functionality

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.handlebars",
+    // "./views/layouts/**/*.handlebars",
+    "./public/**/*.html",
+    "./public/js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
