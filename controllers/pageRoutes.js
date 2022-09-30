e8e793a8c9c5d5dee5fef93d02c09d4ca54a28d0
Dan Kelly <dpk5e7@yahoo.com>
2022-09-29 20:52:56 -0600

    Update pageRoutes.js

--- controllers/pageRoutes.js
+++ controllers/pageRoutes.js
@@ -186,6 +186,19 @@ router.get("/profile", isAuthenticated, async (req, res) => {
   }
 });
 
+// GET addEV
+router.get("/addEV", isAuthenticated, async (req, res) => {
+  try {
+    res.render("addEV", {
+      logged_in: req.session.logged_in,
+      is_admin: req.session.is_admin,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 // GET the change password page
 router.get("/changePassword", isAuthenticated, async (req, res) => {
   try {
