8fdb2dc9e02730f1cd669e7351f213397b0bd010
Dan Kelly <dpk5e7@yahoo.com>
2022-10-03 21:56:54 -0600

    Lockout user

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,23 +1,24 @@
 const saveAddressData = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const strAddress = document.querySelector("#txtAddress").value.trim();
+  const strAddress = document.querySelector("#txtAddress").value.trim();
 
-    if (strAddress) {
-        const response = await fetch("/api/profile", {
-            method: "POST",
-            body: JSON.stringify({ address: strAddress }),
-            headers: { "Content-Type": "application/json"},
-        });
+  if (strAddress) {
+    const response = await fetch("/api/profile", {
+      method: "POST",
+      body: JSON.stringify({ address: strAddress }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-        if(response.ok){
-            alert("Address saved!");
-        } else {
-            alert("Failed to save address.");
-        }
+    if (response.ok) {
+      alert("Address saved!");
+      document.location.replace("/dashboard");
+    } else {
+      alert("Failed to save address.");
     }
+  }
 };
 
 document
-    .querySelector("#saveAddress")
-    .addEventListener("submit", saveAddressData);
+  .querySelector("#saveAddress")
+  .addEventListener("submit", saveAddressData);
