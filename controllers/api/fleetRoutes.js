89ef3798e409ea82f67fb05105a8d77fc84bf13b
Drew <dlederman69@gmail.com>
2022-10-03 19:15:22 -0600

    its fine

--- controllers/api/fleetRoutes.js
+++ controllers/api/fleetRoutes.js
@@ -12,23 +12,6 @@ router.post("/", isAuthenticated, async (req, res) => {
     res.status(400).json(err);
   }
 });
-// ! what is the path (url) to get to this point????  local3001/controller/api/fleetroutes
-router.get("/", isAuthenticated, async (req, res) => {
-  try {
-   const selectObj = await ElectricVehicle.findAll({
-    attributes: ["model_year", "manufacturer_name", "model"]
-   });
-   res.render('addEV', { 
-    selectObj, 
-    logged_in: req.session.logged_in 
-  // res.json(selectObj)
-  });
-  console.log(selectObj)
-  } catch (err) {
-
-      res.status(420).json(err)
-  }
-});
 
 // Remove an EV from the fleet
 router.delete("/:id", isAuthenticated, async (req, res) => {
